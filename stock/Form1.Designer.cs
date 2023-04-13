40d91100dc241f42399e91a0aa9ecbbf3fb21045
wubox322 <kirill.makeev2002@yandex.ru>
2023-04-13 23:19:11 +0300

    yraa

--- stock/Form1.Designer.cs
+++ stock/Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column3,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(49, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(499, 343);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(46, 55);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(111, 13);
+            this.labelCount.TabIndex = 1;
+            this.labelCount.Text = "Количество записей";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Image = ((System.Drawing.Image)(resources.GetObject("Column2.Image")));
+            this.Column2.MinimumWidth = 100;
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column2.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewImageColumn Column2;
     }
 }
 
--- stock/Form1.cs
+++ stock/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace stock
 {
+    
     public partial class Form1 : Form
     {
+        //SqlConnection db = new SqlConnection(@"Data Source = DESKTOP - LT8T67N; Initial Catalog = user02; Persist Security info = True");
+        SqlConnection db = new SqlConnection(@"Data Source=DESKTOP-LT8T67N;Initial Catalog=user02;Integrated Security=True;");
+        string a;
+        int b;
+        int l;
+        int j = 0;
+        List<int> ff = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            db.Open();
+            
+
+            string table = "select *from Fish";
+            SqlCommand command = new SqlCommand(table, db);
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<string[]> data = new List<string[]>();
+
+            while (reader.Read())
+            {
+                data.Add(new string[2]);
+                ff.Add(0);
+
+                data[data.Count - 1][0] = reader[0].ToString();
+                data[data.Count - 1][1] = reader[1].ToString();
+                if(Convert.ToInt32(data[data.Count - 1][1]) < 40)
+                {
+                    ff[data.Count - 1] = 1;
+                    j = Convert.ToInt32(data[data.Count - 1][1]);
+                    l = data.Count - 1;
+                }
+            }
+            a = labelCount.Text;
+            reader.Close();
+            db.Close();
+
+            foreach (string[] s in data)
+                dataGridView1.Rows.Add(s);
+                b = dataGridView1.Rows.Count - 1;
+                labelCount.Text = a + "\t" + data.Count + " из " + b;
+            for (int i = 0; i < ff.Count; i++)
+                if (ff[i] == 1)
+                {
+                    dataGridView1.Rows[i].Cells[1].Style.BackColor = Color.FromArgb(255, 0, 0);
+                    dataGridView1.Rows[i].Cells[1].Style.Font = new Font(this.Font, FontStyle.Strikeout);
+                }
+                
         }
     }
 }
